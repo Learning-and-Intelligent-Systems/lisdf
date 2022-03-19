30215e0fcf59df8fefc870b870ef26fd1208648e
Nishanth Kumar <nishanth.kumar20@gmail.com>
2022-03-19 13:01:46 -0400

    sets up basic requirements, development cycle, and ci

---
+++ src/main.py
@@ -0,0 +1,6 @@
+"""Main entry point for running conversion code."""
+
+
+def main():
+    """Placeholder - currently does nothing interesting"""
+    return "placeholder string"
---
+++ tests/test_main.py
@@ -0,0 +1,8 @@
+"""Tests for main.py."""
+
+from lisdf.src import main
+
+
+def test_main():
+    """Tests for main method."""
+    assert main.main() == "placeholder string"
