fbb1072e17893ce2f818b00c81932411ae172da5
William Shen <william.w.y.shen@gmail.com>
2022-03-22 13:52:14 -0400

    Add a plane

--- drake_robot_position_controller.py
+++ drake_robot_position_controller.py
@@ -1,5 +1,6 @@
-from pydrake.systems.framework import LeafSystem, BasicVector
 import numpy as np
+from pydrake.systems.framework import BasicVector, LeafSystem
+
 
 class DrakeRobotPositionController(LeafSystem):
     def __init__(self, poses):
@@ -11,15 +12,15 @@ class DrakeRobotPositionController(LeafSystem):
         """
         LeafSystem.__init__(self)
         self.poses = poses
-        self.dimensionality = 2*poses[0].shape[0]
+        self.dimensionality = 2 * poses[0].shape[0]
         print("dim: ", self.dimensionality)
-        self.DeclareVectorOutputPort("robot_qv_des", BasicVector(self.dimensionality), self.CalcOutput)
+        self.DeclareVectorOutputPort(
+            "robot_qv_des", BasicVector(self.dimensionality), self.CalcOutput
+        )
 
     def CalcOutput(self, context, output):
-        pose_idx = max(len(self.poses) - 1, context.get_time()/2.0)
+        pose_idx = max(len(self.poses) - 1, context.get_time() / 2.0)
         q_des = self.poses[pose_idx]
         v_des = np.zeros_like(self.poses[pose_idx])
         qv_des = np.concatenate((q_des, v_des))
         output.SetFromVector(qv_des)
-
-
