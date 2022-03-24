28b7a2b210906eceab07e62450d5489b3110b5e4
William Shen <william.w.y.shen@gmail.com>
2022-03-24 11:47:10 -0400

    Trying to clean up

--- lisdf/parsing/parse_sdf.py
+++ lisdf/parsing/parse_sdf.py
@@ -3,50 +3,48 @@ import os
 from lisdf.parsing.sdf import SDF, Collision, Link, Mesh, Visual
 
 
-def inject_absolute_path(sdf: SDF, model_path: str):
+def _handle_component(component, model_path: str) -> None:
+    """
+    Handle component and inject URI into link component geometry
+    """
+    if isinstance(component, Link):
+        for link_component in component.aggregate_order:
+            # TODO: are there other types we need to consider?
+            if isinstance(link_component, (Collision, Visual)) and isinstance(
+                link_component.geometry, Mesh
+            ):
+                print("yup")
+                link_component.geometry.uri = os.path.join(
+                    model_path, link_component.geometry.uri
+                )
 
+
+def inject_absolute_path(sdf: SDF, model_path: str) -> SDF:
     """
     This function replace relative paths to object and material
     files with absolute paths so the sdf object is self-contained.
     """
-    for wi, world in enumerate(sdf.aggregate_order):
+    for world in sdf.aggregate_order:
         for model in world.models:
             for component in model.aggregate_order:
-                if isinstance(component, Link):
-                    for link_component in component.aggregate_order:
-                        if isinstance(link_component, Collision):
-                            if (
-                                "geometry" in link_component.__dict__.keys()
-                                and isinstance(link_component.geometry, Mesh)
-                            ):
-                                link_component.geometry.uri = os.path.join(
-                                    model_path, link_component.geometry.uri
-                                )
-                        if isinstance(link_component, Visual):
-                            if (
-                                "geometry" in link_component.__dict__.keys()
-                                and isinstance(link_component.geometry, Mesh)
-                            ):
-                                link_component.geometry.uri = os.path.join(
-                                    model_path, link_component.geometry.uri
-                                )
-
+                _handle_component(component, model_path)
     return sdf
 
 
-def load_sdf(model_directory: str):
-    sdf_path = os.path.join("models", model_directory)
+def load_sdf(model_name: str, models_dir: str = "models") -> SDF:
+    sdf_path = os.path.join(models_dir, model_name)
     with open(sdf_path) as f:
         xml_str = f.read()
         sdf = SDF.from_xml_string(xml_str)
-        for wi, world in enumerate(sdf.aggregate_order):
+        for world in sdf.aggregate_order:
+            # Load all the includes in the world
             for include in world.includes:
                 include_model = load_sdf(include.model_name)
-                sdf.aggregate_order[wi].models.append(include_model)
-    return inject_absolute_path(sdf, model_directory)
+                world.models.append(include_model)
+    return inject_absolute_path(sdf, model_name)
 
 
 if __name__ == "__main__":  # pragma: no cover
-    sdf_test = "mud_test"
+    sdf_test = "mud_test/model.sdf"
     sdf_results = load_sdf(sdf_test)
-    print(sdf_results)
+    # print(sdf_results)
