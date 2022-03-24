f45f99f76ddb903b6156eaf799cd5a4c64475777
Aidan Curtis <curtisa@mit.edu>
2022-03-24 12:23:46 -0400

    injection test

--- tests/test_parsing/test_parse_sdf.py
+++ tests/test_parsing/test_parse_sdf.py
@@ -6,3 +6,13 @@ def test_recursive_sdf_parsing():
     world = parsed_sdf.aggregate_order[0]
     assert len(world.includes) == 5
     assert len(world.models) == 7
+
+
+
+def test_m0m():
+    parsed_sdf = load_sdf("m0m/model.sdf")
+    world = parsed_sdf.aggregate_order[0]
+    assert len(world.includes) == 0
+    assert len(world.models) == 6
+
+test_m0m()
