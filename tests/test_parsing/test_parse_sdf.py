81b8910cf1f2eb979156230d9dc0c2357ed3d4d6
Aidan Curtis <curtisa@mit.edu>
2022-03-25 12:05:52 -0400

    update test case

--- tests/test_parsing/test_parse_sdf.py
+++ tests/test_parsing/test_parse_sdf.py
@@ -1,4 +1,8 @@
+from collections import Counter
+
 from lisdf.parsing.parse_sdf import load_sdf
+from lisdf.parsing.sdf import Model
+from lisdf.parsing.urdf import Robot
 
 
 def test_recursive_sdf_parsing():
@@ -26,5 +30,14 @@ def test_m0m():
 def test_partnet_urdf():
     parsed_sdf = load_sdf("partnet_mobility_test/model.sdf")
     world = parsed_sdf.aggregate_order[0]
+
+    assert Counter([type(model) for model in world.models]) == Counter(
+        [
+            Model,
+            Model,
+            Robot,
+        ]
+    )
+
     assert len(world.includes) == 1
     assert len(world.models) == 3
