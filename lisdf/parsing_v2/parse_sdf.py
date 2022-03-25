08c7537fc5710da5d2214e1a4f5d78664ab7d08d
Jiayuan Mao <maojiayuan@gmail.com>
2022-03-24 20:07:51 -0500

    [jiayuan] wrong file

--- lisdf/parsing_v2/parse_sdf.py
+++
@@ -1,6 +0,0 @@
-from lisdf.parsing_v2.sdf import load_sdf
-
-if __name__ == "__main__":  # pragma: no cover
-    sdf_test = "mud_test.sdf"
-    sdf_results = load_sdf(sdf_test)
-    print(sdf_results)
