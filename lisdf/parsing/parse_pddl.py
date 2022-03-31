6efc799b5e7b2669190e673cf15224945e74e78a
Nishanth Kumar <nishanth.kumar20@gmail.com>
2022-03-31 13:10:41 -0400

    initial pddl parser use and implementation!

---
+++ lisdf/parsing/parse_pddl.py
@@ -0,0 +1,41 @@
+from typing import Dict, List, Set, Tuple
+
+import pddlpy
+
+
+def parse_pddl(
+    domain_file_path: str, problem_file_path: str
+) -> Tuple[List[List[str]], List[List[str]], Dict[str, Set[str]]]:
+    """
+    Parses the domain and problem PDDL files provided string paths
+    to each of these.
+
+    :return a tuple of the following:
+        - init_atoms_strs: a list of list of strings representing
+          the init atoms, where each inner list represents
+          a ground atom. The first element of the inner list is the
+          name of the predicate, and the following elements correspond
+          to object names that are used to ground the predicate
+        - goal_atoms_strs: same as above, but for the goal atoms
+        - type_to_object_dict: a dictionary mapping a string representing
+          each type to a set of strings representing the objects of that
+          type
+    """
+    dom_prob = pddlpy.DomainProblem(domain_file_path, problem_file_path)
+    init_atoms = dom_prob.initialstate()
+    init_atoms_strs = [atom.predicate for atom in init_atoms]
+    goal_atoms = dom_prob.goals()
+    goal_atoms_strs = [atom.predicate for atom in goal_atoms]
+    obj_to_type_dict = dom_prob.worldobjects()
+    type_to_object_dict: Dict[str, Set[str]] = {}
+    for obj in obj_to_type_dict.keys():
+        if type_to_object_dict.get(obj_to_type_dict[obj]) is None:
+            type_to_object_dict[obj_to_type_dict[obj]] = set([obj])
+        else:
+            type_to_object_dict[obj_to_type_dict[obj]].add(obj)
+
+    return (init_atoms_strs, goal_atoms_strs, type_to_object_dict)
+
+
+if __name__ == "__main__":
+    parse_pddl("examples/pddl/clutter.pddl", "examples/pddl/clutter_prob01.pddl")
