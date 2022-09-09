602b6ba4b1bfc5e58040dc9b05ab01b424bde1b6
Tom Silver <tomssilver@gmail.com>
2022-09-09 18:12:52 -0400

    commit

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
             "pytest-cov",
             "mock",
             # LISdf models (TODO: remove commit after merging lisdf-models PR)
-            "lisdf_models@git+https://github.com/Learning-and-Intelligent-Systems/lisdf-models.git@2e246d6",  # noqa: E501
+            "lisdf_models@git+https://github.com/Learning-and-Intelligent-Systems/lisdf-models.git@a8da4e8",  # noqa: E501
         ]
     },
 )
